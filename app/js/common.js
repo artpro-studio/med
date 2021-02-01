e5a358f997e3ebadfe79463a6bbc02bbce42dc7c
andreyi96 <ya.andreyi96@yandex.ru>
2021-02-01 23:12:10 +0300

    init

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,74 @@
-$(function() {
+window.onload = function(){
+    setTimeout(function(){
+        let arrIframe = document.querySelectorAll('iframe');
+        arrIframe.forEach(forIframe => {
+            let frameSrc = forIframe.getAttribute('data-src');
+            forIframe.setAttribute('src', frameSrc);
+        });
+    },4000);
+}
 
-	// Custom JS
+const images = document.querySelectorAll('img')
+const itemsBg = document.querySelectorAll('.imgLoad')
 
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+}
+
+function handlerImg(dataImg, observer){
+    dataImg.forEach(forImg => {
+        if(forImg.intersectionRatio > 0){
+            loadImages(forImg.target, false);
+        }
+    })
+}
+function handlerBG(dataImg, observer){
+    dataImg.forEach(forImg => {
+        if(forImg.intersectionRatio > 0){
+            loadImages(forImg.target, true);
+        }
+    })
+}
+
+function loadImages(imgage, type ){
+    if(type == false){
+        imgage.src = imgage.getAttribute('data-src');
+    }
+    if(type == true){
+        imgage.style.backgroundImage = `url(${imgage.getAttribute('data-bg')}`
+    }
+}
+
+const observer = new IntersectionObserver(handlerImg, options)
+const observerBG = new IntersectionObserver(handlerBG, options)
+
+images.forEach(img => {
+    observer.observe(img)
+})
+itemsBg.forEach(item => {
+    observerBG.observe(item)
+})
+
+$(function () {
+    $('.js--burger').on('click', function () {
+        $('.js--burger').toggleClass('active');
+        $('.js--mobile_header').toggleClass('active');
+    });
+    $('.js--mobile-search').on('click', function (e) {
+        e.preventDefault();
+        $('.js--mobile-search-form').toggleClass('active');
+    });
+    $(document).on('scroll', function (e) {
+        if ($(window).scrollTop() > 10) {
+            $('.js--header').addClass('scroll');
+        } else {
+            $('.js--header').removeClass('scroll');
+        }
+    });
+    $('.js--head__slider').slick({
+        prevArrow: false,
+        nextArrow: '.js--head-slider-next-arrow',
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -45,6 +45,8 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-1.8.1/slick/slick.min.js',
+		'app/libs/fancybox/jquery.fancybox.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
